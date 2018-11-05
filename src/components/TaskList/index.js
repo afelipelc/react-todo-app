08be149e542e92600d10f4545805c75a2ab8d754
AFelipe <afelipelc@gmail.com>
2018-11-05 11:46:37 -0600

    todo items from array

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import './index.css';
 
 class TaskList extends React.Component {
+  todos = [
+    {
+      id: 1,
+      description: 'Create component',
+    },
+    {
+      id: 2,
+      description: 'Use component',
+    },
+    {
+      id: 3,
+      description: 'Add state to component',
+    }
+  ];
+
+  renderItemsArray = () => {
+    const items = [];
+    this.todos.forEach((item) => {
+      items.push(<li key={item.id}>{item.description}</li>);
+    });
+    return items;
+  }
+
   render() {
     return (
       <div className="taskList">
-        <ul>
-          <li>Instalar nodeJS</li>
-          <li>Instalar npm</li>
-          <li>Instalar create-react-app</li>
-          <li>Crear app React</li>
+        <ul className="listItems">
+          {this.renderItemsArray()}
         </ul>
       </div>
     );
