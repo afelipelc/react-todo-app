63ee657fca3eaa6c16acbdc2e15c156a6d3b98f0
AFelipe <afelipelc@gmail.com>
2018-11-05 12:00:27 -0600

    load todos from state to props

--- src/App.js
+++ src/App.js
@@ -7,11 +7,34 @@ import CreateTask from './components/CreateTask';
 import TaskList from './components/TaskList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          description: 'Create component',
+        },
+        {
+          id: 2,
+          description: 'Use component',
+        },
+        {
+          id: 3,
+          description: 'Add state to component',
+        }
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <CreateTask />
-        <TaskList />
+        <TaskList
+          todos={this.state.todos}
+        />
       </div>
     );
   }
--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -2,34 +2,19 @@ import React from 'react';
 import './index.css';
 
 class TaskList extends React.Component {
-  todos = [
-    {
-      id: 1,
-      description: 'Create component',
-    },
-    {
-      id: 2,
-      description: 'Use component',
-    },
-    {
-      id: 3,
-      description: 'Add state to component',
-    }
-  ];
+  renderItems = () => {
+    // destructing
+    const { todos } = this.props;
 
-  renderItemsArray = () => {
-    const items = [];
-    this.todos.forEach((item) => {
-      items.push(<li key={item.id}>{item.description}</li>);
-    });
-    return items;
+    // map array to <li> array
+    return todos.map((item) => <li key={item.id}>{item.description}</li>);
   }
 
   render() {
     return (
       <div className="taskList">
         <ul className="listItems">
-          {this.renderItemsArray()}
+          {this.renderItems()}
         </ul>
       </div>
     );
